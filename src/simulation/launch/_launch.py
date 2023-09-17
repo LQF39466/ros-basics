945f2f15e678235f20b3d3aae86b271e2d38781a
lqf <leiqf@foxmail.com>
2023-09-17 21:28:26 +0800

    urdf model can now be loaded into rivz

---
+++ src/simulation/launch/_launch.py
@@ -0,0 +1,49 @@
+import os
+from launch import LaunchDescription
+from launch.substitutions import LaunchConfiguration
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import Command
+from launch_ros.parameter_descriptions import ParameterValue
+
+
+def generate_launch_description():
+    package_name = 'simulation'
+    urdf_name = "simulation.urdf"
+
+    ld = LaunchDescription()
+    pkg_share = FindPackageShare(package=package_name).find(package_name)
+    urdf_model_path = os.path.join(pkg_share, f'urdf/{urdf_name}')
+    default_rviz_config_path = os.path.join(pkg_share ,'rviz/urdf.rviz')
+
+    rviz_arg = DeclareLaunchArgument(name='rvizconfig', default_value=str(default_rviz_config_path),
+                                     description='Absolute path to rviz config file')
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        arguments=[urdf_model_path]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher_gui',
+        executable='joint_state_publisher_gui',
+        name='joint_state_publisher_gui',
+        arguments=[urdf_model_path]
+    )
+
+    rviz2_node = Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        output='screen',
+        arguments=['-d', LaunchConfiguration('rvizconfig')]
+    )
+
+    ld.add_action(rviz_arg)
+    ld.add_action(robot_state_publisher_node)
+    ld.add_action(joint_state_publisher_node)
+    ld.add_action(rviz2_node)
+
+    return ld
