8da1f2c27024e392016970511a4cb98fa8005196
Your Name <1521226176@qq.com>
2023-12-27 20:51:40 +0800

    replaced the simulation.urdf with mycar.urdf

--- src/simulation/launch/_launch.py
+++ src/simulation/launch/_launch.py
@@ -4,13 +4,11 @@ from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 from launch.actions import DeclareLaunchArgument
-from launch.substitutions import Command
-from launch_ros.parameter_descriptions import ParameterValue
 
 
 def generate_launch_description():
     package_name = 'simulation'
-    urdf_name = "simulation.urdf"
+    urdf_name = 'mycar.urdf'
 
     ld = LaunchDescription()
     pkg_share = FindPackageShare(package=package_name).find(package_name)
@@ -27,6 +25,13 @@ def generate_launch_description():
     )
 
     joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        arguments=[urdf_model_path]
+    )
+
+    joint_state_publisher_gui_node = Node(
         package='joint_state_publisher_gui',
         executable='joint_state_publisher_gui',
         name='joint_state_publisher_gui',
@@ -44,6 +49,7 @@ def generate_launch_description():
     ld.add_action(rviz_arg)
     ld.add_action(robot_state_publisher_node)
     ld.add_action(joint_state_publisher_node)
+    ld.add_action(joint_state_publisher_gui_node)
     ld.add_action(rviz2_node)
 
     return ld
