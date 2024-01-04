68d0a4cee2444b3f25b8274f419f0b4470f22c39
superfuckergyc <104135371+superfuckergyc@users.noreply.github.com>
2024-01-04 18:07:17 +0800

    Update subscriber.cpp

--- src/comm-basics/src/subscriber.cpp
+++ src/comm-basics/src/subscriber.cpp
@@ -2,30 +2,52 @@
 
 #include "rclcpp/rclcpp.hpp"
 #include "std_msgs/msg/string.hpp"
+#include "sensor_msgs/msg/image.hpp"
+
+#include <opencv2/opencv.hpp>
+
 using std::placeholders::_1;
 
-class MinimalSubscriber : public rclcpp::Node
+class TextVideoSubscriber : public rclcpp::Node
 {
-  public:
-    MinimalSubscriber()
-    : Node("minimal_subscriber")
+public:
+  TextVideoSubscriber()
+    : Node("text_video_subscriber")
+  {
+    text_subscription_ = this->create_subscription<std_msgs::msg::String>(
+      "text_topic", 10, std::bind(&TextVideoSubscriber::text_topic_callback, this, _1));
+    video_subscription_ = this->create_subscription<sensor_msgs::msg::Image>(
+      "video_topic", 10, std::bind(&TextVideoSubscriber::video_topic_callback, this, _1));
+  }
+
+private:
+  void text_topic_callback(const std_msgs::msg::String::SharedPtr msg) const
+  {
+    RCLCPP_INFO(this->get_logger(), "Received text: '%s'", msg->data.c_str());
+  }
+
+  void video_topic_callback(const sensor_msgs::msg::Image::SharedPtr msg) const
+  {
+    cv::Mat frame(msg->height, msg->width, CV_8UC3, const_cast<unsigned char*>(msg->data.data()), msg->step);
+
+    if (frame.empty())
     {
-      subscription_ = this->create_subscription<std_msgs::msg::String>(
-      "topic", 10, std::bind(&MinimalSubscriber::topic_callback, this, _1));
+      RCLCPP_ERROR(this->get_logger(), "Failed to create video frame");
+      return;
     }
 
-  private:
-    void topic_callback(const std_msgs::msg::String & msg) const
-    {
-      RCLCPP_INFO(this->get_logger(), "I heard: '%s'", msg.data.c_str());
-    }
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
+    cv::imshow("Video", frame);
+    cv::waitKey(1);
+  }
+
+  rclcpp::Subscription<std_msgs::msg::String>::SharedPtr text_subscription_;
+  rclcpp::Subscription<sensor_msgs::msg::Image>::SharedPtr video_subscription_;
 };
 
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
-  rclcpp::spin(std::make_shared<MinimalSubscriber>());
+  rclcpp::spin(std::make_shared<TextVideoSubscriber>());
   rclcpp::shutdown();
   return 0;
 }
